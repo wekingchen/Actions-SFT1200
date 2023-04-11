236982903d37fabb1485d0c6288b57c95c955649
wekingchen <weking.chen@gmail.com>
2023-04-11 18:31:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,6 +11,7 @@
 #
 
 wget https://github.com/coolsnowwolf/lede/raw/master/include/meson.mk  -P ./include/
+wget https://github.com/coolsnowwolf/lede/raw/master/include/openssl-engine.mk  -P ./include/
 
 # Uncomment a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
