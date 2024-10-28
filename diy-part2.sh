8ee34e098659ff0f084592d4f811bca6ce13b220
wekingchen <weking.chen@gmail.com>
2024-10-28 18:47:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -50,6 +50,12 @@ cp -r aliyundrive-webdav/openwrt/luci-app-aliyundrive-webdav feeds/luci2/applica
 rm -rf aliyundrive-webdav
 
 # 修改frp版本为官网最新v0.61.0 https://github.com/fatedier/frp 格式：https://codeload.github.com/fatedier/frp/tar.gz/v${PKG_VERSION}?
+rm -rf feeds/packages2/net/frp
+wget https://github.com/coolsnowwolf/packages/archive/0f7be9fc93d68986c179829d8199824d3183eb60.zip -O OldPackages.zip
+unzip OldPackages.zip
+cp -r packages-0f7be9fc93d68986c179829d8199824d3183eb60/net/frp feeds/packages2/net/
+rm -rf OldPackages.zip packages-0f7be9fc93d68986c179829d8199824d3183eb60s
+
 sed -i 's/PKG_VERSION:=0.53.2/PKG_VERSION:=0.61.0/' feeds/packages2/net/frp/Makefile
 sed -i 's/PKG_HASH:=ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/PKG_HASH:=c06a11982ef548372038ec99a6b01cf4f7817a9b88ee5064e41e5132d0ccb7e1/' feeds/packages2/net/frp/Makefile
 
