a326675a1cfc24b3f88f3c167a20f7ce639d6ee9
wekingchen <weking.chen@gmail.com>
2025-10-24 19:02:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 wget https://github.com/coolsnowwolf/lede/raw/master/include/meson.mk  -P ./include/
 wget https://codeberg.org/aparcar/openwrt/src/branch/apk/include/openssl-engine.mk  -P ./include/
 
+rm -rf feeds/gl_feed_common/golang
+
 # Uncomment a feed source
 sed -i "/helloworld/d" "feeds.conf.default"
 
