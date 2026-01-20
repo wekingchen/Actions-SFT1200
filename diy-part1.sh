8041c939a42041b8a0962fba514d7773a02eb3d8
wekingchen <weking.chen@gmail.com>
2026-01-20 11:31:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@ sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git gl https://github.com/gl-inet/gl-feeds.git;18.06" >> "feeds.conf.default"
 echo "src-git luci2 https://github.com/coolsnowwolf/luci" >> "feeds.conf.default"
 echo "src-git packages2 https://github.com/coolsnowwolf/packages" >> "feeds.conf.default"
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git PWpackages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git PWluci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git;master" >> "feeds.conf.default"
 
 ./scripts/feeds clean
